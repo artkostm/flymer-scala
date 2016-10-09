087e879dc85606ba0617c97801ad15a4d4bf5067
Artsiom <artkostm@gmail.com>
2016-10-09 22:01:39 +0300

    added login method

--- src/main/scala/com/artkostm/flymer/login/Login.scala
+++ src/main/scala/com/artkostm/flymer/login/Login.scala
@@ -1,5 +1,9 @@
 package com.artkostm.flymer.login
 
+import android.os.{Handler, Message}
+import android.util.Log
+import org.jsoup.{Connection, Jsoup}
+
 /**
   * Created by arttsiom.chuiko on 08/10/2016.
   */
@@ -9,6 +13,43 @@ object Login {
       val m = (n << 5) - n + g
       m & m
     })
+  }
+
+  def AttemptLogin(email: String, pass: String): String = {
+    try{
+      val connection = Jsoup.connect("http://flymer.ru")
+      connection.userAgent("Mozilla").method(Connection.Method.GET)
+      val doc = connection.get()
+      val cookies = connection.response().cookies()
+      val fkey = doc.select("input[name='fkey']").first().attr("value")
+      val lkey = doc.select("input[name='lkey']").first().attr("value")
+      Log.i("scala", "FKEY:" + fkey + ", LKEY:" + lkey)
+      val dkey = Dkey(fkey)
+      cookies.put("fkey", fkey)
+
+      val ac = Jsoup.connect("http://flymer.ru/req/login?ts="+System.currentTimeMillis())
+        .data("pass", pass)
+        .data("email", email)
+        .data("fkey",fkey)
+        .data("lkey",lkey)
+        .data("dkey",String.valueOf(dkey))
+        .header("Content-Type","application/x-www-form-urlencoded")
+        .header("Connection","keep-alive")
+        .cookies(cookies)
+        .method(Connection.Method.POST)
+        .execute().cookie("ac");
+
+      cookies.put("lkey", lkey);
+      cookies.put("dkey", String.valueOf(dkey))
+
+      "ac:"+ac+"\nsid:"+cookies.get("sid")
+    } catch {
+      case e: Exception => {
+        Log.i("scala", e.getMessage, e)
+        e.printStackTrace()
+        e.getMessage }
+    }
+
   }
 }
 
