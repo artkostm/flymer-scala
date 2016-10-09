e56afa50f86d6d06e9e9db43293783bc324e532f
Artsiom <artkostm@gmail.com>
2016-10-09 23:14:57 +0300

    update code using new implicits

--- src/main/scala/com/artkostm/flymer/FirstActivity.scala
+++ src/main/scala/com/artkostm/flymer/FirstActivity.scala
@@ -1,8 +1,7 @@
 package com.artkostm.flymer
 
 import android.app.Activity
-import android.os.{Bundle, Handler, Message}
-import android.util.Log
+import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget._
@@ -11,12 +10,13 @@ import com.artkostm.flymer.view.Tweaks
 import macroid.Contexts
 import macroid.FullDsl._
 import macroid.contrib.LpTweaks._
+import com.artkostm.flymer.utils.ViewHelper._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 /**
- * Created by arttsiom.chuiko on 03/10/2016.
+ * Created by artsiom.chuiko on 03/10/2016.
  */
 class FirstActivity extends Activity with Contexts[Activity] {
   override def onCreate(savedInstanceState: Bundle): Unit = {
@@ -26,24 +26,17 @@ class FirstActivity extends Activity with Contexts[Activity] {
 
     val mainView = l[LinearLayout] (
       w[EditText] <~ matchWidth,
-      w[EditText] <~ matchWidth <~ Tweaks.password(),
-      w[Button] <~ wire(label) <~ text("This is Test layout for Flymer login activity")
+      w[EditText] <~ matchWidth <~ Tweaks.password,
+      w[Button] <~ matchWidth <~ wire(label) <~ text("Log in")
     ) <~ vertical
 
     setContentView(mainView.get)
 
-    label.get.setOnClickListener {
-      new OnClickListener {
-        override def onClick(v: View): Unit = {
-          
-          Future {
-            AttemptLogin()
-          } map { value =>
-            runOnUiThread(new Runnable {
-              override def run(): Unit = Toast.makeText(FirstActivity.this, value, Toast.LENGTH_LONG).show()
-            })
-          }
-        }
+    label.get.setOnClickListener { source: View =>
+      Future {
+        AttemptLogin("artkostm@gmail.com", "061994art")
+      } map { value =>
+        runOnUiThread { Toast.makeText(FirstActivity.this, value, Toast.LENGTH_LONG).show() }
       }
     }
   }
