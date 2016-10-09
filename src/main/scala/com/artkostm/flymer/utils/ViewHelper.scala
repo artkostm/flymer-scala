a60dd011f511d5f1d5fd78ceb40b5c12cd649300
Artsiom <artkostm@gmail.com>
2016-10-09 23:14:17 +0300

    added utility implicits

--- src/main/scala/com/artkostm/flymer/utils/ViewHelper.scala
+++ src/main/scala/com/artkostm/flymer/utils/ViewHelper.scala
@@ -3,10 +3,15 @@ package com.artkostm.flymer.utils
 import android.view.View
 
 /**
-  * Created by arttsiom.chuiko on 08/10/2016.
+  * Created by artsiom.chuiko on 08/10/2016.
   */
 object ViewHelper {
-  implicit def onClick(handler: View => Unit) = new View.OnClickListener() {
-    override def onClick(source: View) = handler(source)
+
+  implicit def toRun(handler: => Unit) = new Runnable {
+    override def run(): Unit = handler
+  }
+
+  implicit def toOnClickListener[V <: View](handler: V => Any) = new View.OnClickListener {
+    override def onClick(source: View): Unit = handler(source.asInstanceOf[V])
   }
 }
