4dcccec4b95c77a8f2b9d56502564942ad63d325
Artsiom <artkostm@gmail.com>
2016-10-13 16:13:03 +0300

    some refactoring

---
+++ src/main/scala/com/artkostm/flymer/communication/Flymer.scala
@@ -0,0 +1,8 @@
+package com.artkostm.flymer.communication
+
+/**
+  * Created by arttsiom.chuiko on 13/10/2016.
+  */
+class Flymer {
+
+}
--- src/main/scala/com/artkostm/flymer/communication/login/Login.scala
+++ src/main/scala/com/artkostm/flymer/communication/login/Login.scala
@@ -1,6 +1,5 @@
-package com.artkostm.flymer.login
+package com.artkostm.flymer.communication.login
 
-import android.util.Log
 import org.jsoup.nodes.Document
 import org.jsoup.{Connection, Jsoup}
 
@@ -10,6 +9,7 @@ import scala.util.{Failure, Success, Try}
   * Created by artsiom.chuiko on 08/10/2016.
   */
 object Login {
+
   def Dkey(fkey: String): Int = {
     fkey.foldLeft(0)( (n, g) => {
       val m = (n << 5) - n + g
@@ -19,20 +19,17 @@ object Login {
 
   def AttemptLogin(email: String, pass: String): Try[LoginInfo] = requestLoginPage() match {
     case Success(con) => {
-      Log.i("scala", "Success")
       val doc = con.get
       val cookies = con.response.cookies
       val fkey = getAttr(doc, "input[name='fkey']", "value")
       val lkey = getAttr(doc, "input[name='lkey']", "value")
       val dkey = Dkey(fkey)
-      Log.i("scala", "before account")
       cookies.put("fkey", fkey)
       val ac = requestAccount(email, pass, fkey, lkey, dkey, cookies)
-      Log.i("scala", s"after account: $ac")
       Success(new LoginInfo(ac.get, fkey, cookies.get("sid")))
     }
 
-    case Failure(e) => {Log.i("scala", s"Failure: ${e.getMessage}"); Failure(e)}
+    case Failure(e) => Failure(e)
   }
 
   protected def requestLoginPage(): Try[Connection] = Try(Jsoup.connect("http://flymer.ru").userAgent("Mozilla").method(Connection.Method.GET))
@@ -40,7 +37,7 @@ object Login {
   protected def requestAccount(email: String, pass: String, fkey: String, lkey: String, dkey: Int, cookies: java.util.Map[String, String]): Try[String] = Try(
     Jsoup.connect(s"http://flymer.ru/req/login?ts=${System.currentTimeMillis}").
       data("pass", pass).data("email", email).data("fkey",fkey).data("lkey",lkey).data("dkey",String.valueOf(dkey)).
-      header("Content-Type","application/x-www-form-urlencoded").header("Connection","keep-alive").
+      header("Content-Type", "application/x-www-form-urlencoded").header("Connection", "keep-alive").
       cookies(cookies).
       method(Connection.Method.POST).
       execute().
