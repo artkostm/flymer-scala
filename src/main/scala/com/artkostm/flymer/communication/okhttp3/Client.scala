d7108aaaeddd1fed83a0b98d129645db0359e54a
Artsiom <artkostm@gmail.com>
2016-10-13 17:13:24 +0300

    updated cradle dependencies

--- src/main/scala/com/artkostm/flymer/communication/okhttp3/Client.scala
+++ src/main/scala/com/artkostm/flymer/communication/okhttp3/Client.scala
@@ -1,8 +1,23 @@
 package com.artkostm.flymer.communication.okhttp3
 
+import android.content.Context
+import com.franmontiel.persistentcookiejar.PersistentCookieJar
+import com.franmontiel.persistentcookiejar.cache.SetCookieCache
+import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor
+import okhttp3.OkHttpClient
+
 /**
-  * Created by arttsiom.chuiko on 13/10/2016.
+  * Created by artsiom.chuiko on 13/10/2016.
   */
+object Client {
+
+  def RegisterClient(context: Context): OkHttpClient = {
+    val cookieJar = new PersistentCookieJar(new SetCookieCache, new SharedPrefsCookiePersistor(context))
+    new OkHttpClient()
+  }
+  
+}
+
 class Client {
 
 }
