183781cf5eea816fe24641736e117b7f45817b8a
Artsiom <artkostm@gmail.com>
2016-10-13 16:14:01 +0300

    added new base

--- src/main/scala/com/artkostm/flymer/communication/Flymer.scala
+++ src/main/scala/com/artkostm/flymer/communication/Flymer.scala
@@ -1,8 +1,21 @@
 package com.artkostm.flymer.communication
 
 /**
-  * Created by arttsiom.chuiko on 13/10/2016.
+  * Created by artsiom.chuiko on 13/10/2016.
   */
-class Flymer {
+object Flymer {
+  val BaseUrl = "http://flymer.ru"
+  def LoginUrl(currentTime: Long) = s"$BaseUrl/req/login?ts=$currentTime"
 
+  val Fkey = "fkey"
+  val FkeyCssSelector = s"input[name='$Fkey']"
+  val Lkey = "lkey"
+  val Dkey = "dkey"
+  val Sid = "sid"
+  val LkeyCssSelector = s"input[name='$Lkey']"
+  val Pass = "pass"
+  val Email = "email"
+  val Ac = "ac"
+
+  val UserAgent = "Mozilla"
 }
---
+++ src/main/scala/com/artkostm/flymer/communication/okhttp3/Client.scala
@@ -0,0 +1,8 @@
+package com.artkostm.flymer.communication.okhttp3
+
+/**
+  * Created by arttsiom.chuiko on 13/10/2016.
+  */
+class Client {
+
+}
