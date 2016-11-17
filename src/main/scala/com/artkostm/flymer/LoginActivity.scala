4f2f9ff39dc0f7833e2ded9957331b74788b130c
Artsiom <artkostm@gmail.com>
2016-11-17 18:15:54 +0300

    added test call to the pipeline service

--- src/main/scala/com/artkostm/flymer/LoginActivity.scala
+++ src/main/scala/com/artkostm/flymer/LoginActivity.scala
@@ -1,8 +1,8 @@
 package com.artkostm.flymer
 
 import android.app.Activity
-import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget._
 import com.artkostm.flymer.communication.login.Login.AttemptLogin
@@ -11,10 +11,13 @@ import macroid.Contexts
 import macroid.FullDsl._
 import macroid.contrib.LpTweaks._
 import com.artkostm.flymer.utils.FlymerHelper._
+import com.google.android.gms.gcm.{GcmNetworkManager, PeriodicTask}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
+import com.artkostm.flymer.service.PipelineService
+import com.google.android.gms.common.{ConnectionResult, GoogleApiAvailability, GooglePlayServicesUtil}
 
 /**
  * Created by artsiom.chuiko on 03/10/2016.
@@ -34,18 +37,34 @@ class LoginActivity extends Activity with Contexts[Activity] {
     ) <~ vertical
 
     setContentView(mainView.get)
+    runService
 
     label.get.setOnClickListener { source: View =>
       Future {
         AttemptLogin(email.get.getText, password.get.getText)
       } map { value => value match {
           case Success(loginInfo) => runOnUiThread {
-            Toast.makeText(LoginActivity.this, loginInfo.toString, Toast.LENGTH_LONG).show()
+            Toast.makeText(LoginActivity.this, loginInfo.toString, Toast.LENGTH_LONG).show
 //            getSharedPreferences("CookiePersistence", Context.MODE_PRIVATE).edit().
           }
-          case Failure(e) => runOnUiThread { Toast.makeText(LoginActivity.this, e.getMessage, Toast.LENGTH_LONG).show() }
+          case Failure(e) => runOnUiThread { Toast.makeText(LoginActivity.this, e.getMessage, Toast.LENGTH_LONG).show }
         }
       }
     }
+  }
+
+  private def runService(): Unit = {
+    val gcmManager = GcmNetworkManager.getInstance(LoginActivity.this)
+//    Log.i("SCALA", s"Class: ${classOf[PipelineService]}")
+//    val task = new PeriodicTask.Builder().
+//      setService(classOf[TestService]).
+//      setPeriod(60).
+//      setFlex(10).
+//      setTag("flymer_periodic_task").
+//      setPersisted(true).
+//      build()
+    val resultCode = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(LoginActivity.this)
+    if (ConnectionResult.SUCCESS == resultCode) Toast.makeText(LoginActivity.this, "Founded Google services", Toast.LENGTH_SHORT).show //gcmManager.schedule(task)
+    else Toast.makeText(LoginActivity.this, "Cannot run Google services", Toast.LENGTH_SHORT).show
   }
 }
