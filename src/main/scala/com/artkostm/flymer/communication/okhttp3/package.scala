86dc4956420733827abd5df95040468e08352224
Artsiom <artkostm@gmail.com>
2016-11-21 15:12:05 +0300

    disabled package object

--- src/main/scala/com/artkostm/flymer/communication/okhttp3/package.scala
+++ src/main/scala/com/artkostm/flymer/communication/okhttp3/package.scala
@@ -10,14 +10,14 @@ import scala.concurrent.ExecutionContext
   * Created by artsiom.chuiko on 17/11/2016.
   */
 package object okhttp3 {
-  val Executor = new {
-
-    implicit lazy val Pool = ExecutionContext.fromExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
-
-    lazy val Ui = ExecutionContext.fromExecutor(new Executor {
-      private val handler = new Handler(Looper.getMainLooper)
-
-      override def execute(command: Runnable) = handler.post(command)
-    })
-  }
+//  val Executor = new {
+//
+//    implicit lazy val Pool = ExecutionContext.fromExecutor(AsyncTask.THREAD_POOL_EXECUTOR)
+//
+//    lazy val Ui = ExecutionContext.fromExecutor(new Executor {
+//      private val handler = new Handler(Looper.getMainLooper)
+//
+//      override def execute(command: Runnable) = handler.post(command)
+//    })
+//  }
 }
