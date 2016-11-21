8dedf65df07af7b682c759d6485c258d54e7d0d9
Artsiom <artkostm@gmail.com>
2016-11-21 18:14:28 +0300

    updated manifest & new splash activity

---
+++ src/main/scala/com/artkostm/flymer/SplashActivity.scala
@@ -0,0 +1,20 @@
+package com.artkostm.flymer
+
+import android.app.Activity
+import android.content.Intent
+import android.os.{Bundle, Handler}
+
+/**
+ * Created by artsiom.chuiko on 21/11/2016.
+ */
+class SplashActivity extends Activity {
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    import com.artkostm.flymer.utils.FlymerHelper._
+    new Handler().postDelayed({
+      Thread.sleep(2000)
+      startActivity(new Intent(SplashActivity.this, classOf[LoginActivity]))
+      finish
+    }, 2000)
+  }
+}
