f42b5aabe27cb4c326a60fc5d1c95c47eb760343
Artsiom <artkostm@gmail.com>
2016-11-29 17:25:09 +0300

    added new concept initial implementation

---
+++ src/main/scala/com/artkostm/flymer/utils/Events.scala
@@ -0,0 +1,10 @@
+package com.artkostm.flymer.utils
+
+/**
+  * Created by artsiom.chuiko on 29/11/2016.
+  */
+object Events {
+  case class Event[T](source: T)
+
+  type EventHandler = PartialFunction[Event, Unit]
+}
