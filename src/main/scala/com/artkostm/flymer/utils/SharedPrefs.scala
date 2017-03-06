64bcc8af5c04764d4dafa5c76510c102df38c2e0
Artsiom <artkostm@gmail.com>
2017-03-06 17:40:26 +0300

    add utility class for app specific shared prefs

---
+++ src/main/scala/com/artkostm/flymer/utils/SharedPrefs.scala
@@ -0,0 +1,27 @@
+package com.artkostm.flymer.utils
+
+import android.content.Context
+
+/**
+  * Created by artsiom.chuiko on 06/03/2017.
+  */
+class SharedPrefs(context: Context) {
+  val appPrefs = context.getSharedPreferences("FlymerApp", Context.MODE_PRIVATE)
+
+  import scala.collection.JavaConversions._
+  def load(): Map[String, String] = appPrefs.getAll.entrySet().toMap[String, String]
+
+  def save(prefMap: Map[String, String]): Unit = {
+    val editor = appPrefs.edit()
+    prefMap.foreach { entry => editor.putString(entry._1, entry._2) }
+    editor.apply()
+  }
+
+  def remove(prefMap: Map[String, String]): Unit = {
+    val editor = appPrefs.edit()
+    prefMap.keys.foreach(editor.remove(_))
+    editor.apply()
+  }
+
+  def clear(): Unit = appPrefs.edit().clear().apply()
+}
