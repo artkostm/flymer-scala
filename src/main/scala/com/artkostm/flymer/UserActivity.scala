0e252830a4e21603178eb3332ca3268cc5d8f779
Artsiom <artkostm@gmail.com>
2017-03-06 17:39:53 +0300

    introduce user activity

---
+++ src/main/scala/com/artkostm/flymer/UserActivity.scala
@@ -0,0 +1,16 @@
+package com.artkostm.flymer
+
+import android.app.Activity
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import macroid.Contexts
+
+/**
+ * Created by artsiom.chuiko on 06/03/2017.
+ */
+class UserActivity extends AppCompatActivity with Contexts[Activity]{
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+
+  }
+}
