3b461a63b50e282f5b8d3f6a5af998556f9b6f2b
Artsiom <artkostm@gmail.com>
2017-08-05 16:50:55 +0300

    initial implementation of diode architecture

--- src/main/scala/com/artkostm/flymer/Application.scala
+++ src/main/scala/com/artkostm/flymer/Application.scala
@@ -6,6 +6,7 @@ import android.os.{AsyncTask, Handler, Looper}
 import com.franmontiel.persistentcookiejar.PersistentCookieJar
 import com.franmontiel.persistentcookiejar.cache.SetCookieCache
 import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor
+import diode._
 import okhttp3.OkHttpClient
 
 import scala.concurrent.ExecutionContext
@@ -13,8 +14,12 @@ import scala.concurrent.ExecutionContext
 /**
   * Created by artsiom.chuiko on 20/11/2016.
   */
-class Application extends android.app.Application with HttpClientProvider {
+class Application extends android.app.Application with HttpClientProvider with Circuit[FlymerModel] {
   override def onCreate(): Unit = super.onCreate
+
+  override protected def initialModel: FlymerModel = FlymerModel(User())
+
+  override protected def actionHandler: HandlerFunction = new LoginActionHandler(zoomTo(_.user))
 }
 
 object Application {
@@ -23,7 +28,6 @@ object Application {
 
   lazy val Ui = ExecutionContext.fromExecutor(new Executor {
     private val handler = new Handler(Looper.getMainLooper)
-
     override def execute(command: Runnable) = handler.post(command)
   })
 }
@@ -32,4 +36,13 @@ trait HttpClientProvider { app: android.app.Application =>
   lazy val sharedPrefsCookiePersistor = new SharedPrefsCookiePersistor(app.getApplicationContext)
   lazy val cookieJar = new PersistentCookieJar(new SetCookieCache, sharedPrefsCookiePersistor)
   lazy val okHttpClient = new OkHttpClient.Builder().cookieJar(cookieJar).build()
+}
+
+case class FlymerModel(user: User)
+case class User(email: String = "", password: String = "")
+
+class LoginActionHandler[M](modelRW: ModelRW[M, User]) extends ActionHandler(modelRW) {
+  override protected def handle = {
+    case action: Action => ???
+  }
 }
