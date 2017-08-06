36640b8152576519582e5eed4e6ac9631ffe4c8b
Artsiom <artkostm@gmail.com>
2017-08-06 17:57:58 +0300

    full refactoring

--- src/main/scala/com/artkostm/flymer/LoginActivity.scala
+++ src/main/scala/com/artkostm/flymer/LoginActivity.scala
@@ -1,129 +1,91 @@
 package com.artkostm.flymer
 
-import android.app.{Activity, ProgressDialog}
+import android.app.Activity
 import android.os.Bundle
-import android.support.design.widget.{TextInputEditText, TextInputLayout}
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.AppCompatButton
-import android.text.Html
-import android.util.Patterns
-import android.view.{Gravity, View}
-import android.view.ViewGroup.LayoutParams
 import android.widget._
 import com.artkostm.flymer.communication.login.Login.attemptLogin
 import com.artkostm.flymer.communication.login.{VkCookieInterceptor, VkLoginDialog}
-import com.artkostm.flymer.view.{LoginView, Tweaks}
+import com.artkostm.flymer.view.LoginView
 import macroid.Contexts
-import macroid.FullDsl._
-import macroid.{Ui => uinterface}
 import macroid._
-import com.artkostm.flymer.utils.FlymerImplicits._
 import com.google.android.gms.gcm.{GcmNetworkManager, OneoffTask}
 
 import scala.util.{Failure, Success}
 import com.artkostm.flymer.service.PipelineService
 import com.artkostm.flymer.utils.SharedPrefs
 import com.google.android.gms.common.{ConnectionResult, GoogleApiAvailability}
-import macroid.contrib.{ImageTweaks, TextTweaks}
+import diode._
 
 /**
  * Created by artsiom.chuiko on 03/10/2016.
  */
-class LoginActivity extends AppCompatActivity with Contexts[Activity] {
-
-  var loginBtn = slot[AppCompatButton]
-  var emailSlot = slot[TextInputEditText]
-  var passwordSlot = slot[TextInputEditText]
-  var vkDialogD: VkLoginDialog = _
+class LoginActivity extends AppCompatActivity with Contexts[Activity] with Circuit[LoginModel]{
 
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
     setContentView {
-      Ui.get(LoginView.render(null))
+      Ui.get(LoginView.render(this))
     }
-  }
-
-  lazy val flymerLogin: Ui[Unit] = Ui {
-    if(validate) {
-      val dialog = openDialog()
-      val sharedPrefsPersistor = getApplication.asInstanceOf[Application].sharedPrefsCookiePersistor
-      import com.artkostm.flymer.Application._
-      attemptLogin(emailSlot.get.getText, passwordSlot.get.getText) mapUi { loginInfoTry =>
-        dialog.dismiss()
-        import com.artkostm.flymer.Implicits._
-        loginInfoTry match {
-          case Success(loginInfo) => {
-            sharedPrefsPersistor.saveAll(loginInfo)
-            new SharedPrefs(getApplicationContext).save(Map(UserActivity.Email -> emailSlot.get.getText, UserActivity.Pass -> passwordSlot.get.getText))
-            runService()
-            LoginActivity.this.finish()
-            uinterface.nop
-          }
-          case Failure(e) => toast(e.getMessage) <~ long <~ fry
-        }
-      }
-    }
-  }
-
-  val interceptor = new VkCookieInterceptor {
-    override def onCookieIntercepted(cookieString: String): Unit = {
-      import com.artkostm.flymer.Implicits._
-      getApplication.asInstanceOf[Application].sharedPrefsCookiePersistor.saveAll(cookieString)
-      runService()
-      new SharedPrefs(getApplicationContext).save(Map(UserActivity.Vk -> "ok"))
-      LoginActivity.this.finish()
-    }
-
-    override def onPageStarted(): Unit = if (vkDialogD != null) vkDialogD.dismiss() //maybe just hide and then, in onCookieIntercepted, call dismiss()
-  }
-
-  lazy val vkLogin: Ui[Unit] = Ui {
-    vkDialogD = new VkLoginDialog(LoginActivity.this, interceptor)
-    vkDialogD.show()
-    uinterface.nop
   }
 
   override def onBackPressed(): Unit = moveTaskToBack(true)
 
-  def onLoginFailed(): Unit = loginBtn.get.setEnabled(true)
+  override protected def initialModel: LoginModel = LoginModel(User())
 
-  private def runService(): Unit = {
-    val gcmManager = GcmNetworkManager.getInstance(LoginActivity.this)
-    val task = new OneoffTask.Builder().setService(classOf[PipelineService]).
-      setTag(PipelineService.TagOneOff).
-      setExecutionWindow(5, 20).
-      build()
-    val resultCode = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(LoginActivity.this)
-    if (ConnectionResult.SUCCESS == resultCode) gcmManager.schedule(task)
-    else Toast.makeText(LoginActivity.this, "Cannot run Google services", Toast.LENGTH_SHORT).show
-  }
-
-  import com.artkostm.flymer.Implicits._
-  def validate: Boolean = {
-    val erroneous = List(EmailSlot(emailSlot), PasswordSlot(passwordSlot)).filter(slot => slot match {
-      case EmailSlot(Some(email)) if (email.isEmpty || !Patterns.EMAIL_ADDRESS.matcher(email).matches) => {
-        email.setError(Html.fromHtml("<font color='red'>enter a valid email address</font>"))
-        true
-      }
-      case PasswordSlot(Some(password)) if (password.isEmpty || password.length < 4 || password.length > 20) => {
-        password.setError(Html.fromHtml("<font color='red'>between 4 and 20 alphanumeric characters</font>"))
-        true
-      }
-      case _ => false
-    }).map(_.slot.getOrElse(None[TextInputEditText]))
-    erroneous.foreach(_.requestFocus())
-    erroneous.isEmpty
-  }
-
-  private def openDialog(): ProgressDialog = {
-    val dialog = new ProgressDialog(LoginActivity.this, R.style.AppTheme_Dark_Dialog)
-    dialog.setIndeterminate(true)
-    dialog.setMessage("Authenticating...")
-    dialog.show()
-    dialog
-  }
+  override protected def actionHandler: HandlerFunction = new LoginActionHandler(zoomTo(_.user), this)
 }
 
-case class Slot(slot: Option[TextInputEditText])
-case class EmailSlot(email: Option[TextInputEditText]) extends Slot(email)
-case class PasswordSlot(password: Option[TextInputEditText]) extends Slot(password)
+class LoginActionHandler[M](modelRW: ModelRW[M, User], activity: Activity with Circuit[LoginModel]) extends ActionHandler(modelRW) with VkCookieInterceptor{
+  var vkDialogD: VkLoginDialog = _
+
+  import com.artkostm.flymer.Application._
+  override protected def handle = {
+    case VkLogin => effectOnly(Effect.action {
+      vkDialogD = new VkLoginDialog(activity, this)
+      vkDialogD.show()
+      NoAction
+    })
+    case FlymerLogin(Some(email), Some(password)) => updated(
+      value.copy(email, password), Effect(
+        attemptLogin(email, password).map(attempt => FlymerUser(attempt))
+      )
+    )
+    case FlymerUser(Success(loginInfo)) => effectOnly(Effect.action(SaveLoginInfo(loginInfo)))
+    case FlymerUser(Failure(e)) => effectOnly(Effect.action {
+      Toast.makeText(activity, e.getMessage, Toast.LENGTH_SHORT).show
+      NoAction
+    })
+    case SaveLoginInfo(info) => effectOnly(Effect.action {
+      import com.artkostm.flymer.Implicits._
+      val persistor = activity.getApplication.asInstanceOf[Application].sharedPrefsCookiePersistor
+      persistor.saveAll(info)
+      new SharedPrefs(activity.getApplicationContext).save(Map(UserActivity.Email -> value.email, UserActivity.Pass -> value.password))
+      RunService
+    })
+    case RunService => effectOnly(Effect.action {
+      val gcmManager = GcmNetworkManager.getInstance(activity)
+      val task = new OneoffTask.Builder().setService(classOf[PipelineService]).
+        setTag(PipelineService.TagOneOff).
+        setExecutionWindow(5, 20).
+        build()
+      val resultCode = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(activity)
+      if (ConnectionResult.SUCCESS == resultCode) gcmManager.schedule(task)
+      else Toast.makeText(activity, "Cannot run Google services", Toast.LENGTH_SHORT).show
+      activity.finish()
+      NoAction
+    })
+  }
+
+  override def onCookieIntercepted(cookieString: String): Unit = {
+    import com.artkostm.flymer.Implicits._
+    activity.getApplicationContext.asInstanceOf[Application].sharedPrefsCookiePersistor.saveAll(cookieString)
+    new SharedPrefs(activity.getApplicationContext).save(Map(UserActivity.Vk -> "ok"))
+    activity.dispatch(RunService)
+  }
+
+  override def onPageStarted(): Unit = if (vkDialogD != null) vkDialogD.dismiss() //maybe just hide and then, in onCookieIntercepted, call dismiss()
+}
+
+case class LoginModel(user: User)
+case class User(email: String = "", password: String = "")
