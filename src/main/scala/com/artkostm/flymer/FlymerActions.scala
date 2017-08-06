11e33a0ede8d37309806db555406035659dd0c9a
Artsiom <artkostm@gmail.com>
2017-08-06 17:57:21 +0300

    new actions

--- src/main/scala/com/artkostm/flymer/FlymerActions.scala
+++ src/main/scala/com/artkostm/flymer/FlymerActions.scala
@@ -1,9 +1,15 @@
 package com.artkostm.flymer
 
+import com.artkostm.flymer.communication.login.LoginInfo
 import diode.Action
+
+import scala.util.Try
 
 /**
   * Created by artsiom.chuiko on 05/08/2017.
   */
 case object VkLogin extends Action
 case class FlymerLogin(email: Option[String], password: Option[String]) extends Action
+case class FlymerUser(user: Try[LoginInfo]) extends Action
+case class SaveLoginInfo(info: LoginInfo) extends Action
+case object RunService extends Action
