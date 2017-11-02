2c4039081d6f1a8b7026d20ace614cb366c3ceee
Artsiom <artkostm@gmail.com>
2017-11-03 00:15:54 +0300

    add freestyle example

--- src/main/scala/com/artkostm/flymer/view/LoginView.scala
+++ src/main/scala/com/artkostm/flymer/view/LoginView.scala
@@ -26,6 +26,13 @@ case class PasswordSlot(password: Option[TextInputEditText]) extends Slot {
   override def slot: Option[TextInputEditText] = password
 }
 
+private object ActionIfInvalid {
+  def apply(msg: String): TextInputEditText => Unit = editText => {
+    editText.setError(Html.fromHtml(msg))
+    editText.requestFocus()
+  }
+}
+
 object LoginView {
 
   private var emailSlot = slot[TextInputEditText]
@@ -74,7 +81,23 @@ object LoginView {
   ) <~ Tweaks.fitsSystemWindow(true) <~ Tweaks.fillViewport
 
   import com.artkostm.flymer.Implicits._
+//  import cats._
+//  import cats.data._
+//  import cats.implicits._
   private[view] def validate: Boolean = {
+
+//    (Validated.fromOption[(TextInputEditText) => Unit, Option[TextInputEditText]](emailSlot.map {
+//        case email if (email.isEmpty || !Patterns.EMAIL_ADDRESS.matcher(email).matches) => None
+//        case e => Some(e)
+//      }, ActionIfInvalid("msg")) product Validated.fromOption[(TextInputEditText) => Unit, Option[TextInputEditText]](passwordSlot.map {
+//        case password if (password.isEmpty || password.length < 4 || password.length > 20) => None
+//        case e => Some(e)
+//      }, ActionIfInvalid("msg"))).bimap(_(emailSlot.get), _)
+//
+//
+//    import Validated.valid
+//    (valid[String, String]("1"), valid[String, String]("2")) mapN {_ + _ + _}
+
     val erroneous = List(EmailSlot(emailSlot), PasswordSlot(passwordSlot)).filter(slot => slot match {
       case EmailSlot(Some(email)) if (email.isEmpty || !Patterns.EMAIL_ADDRESS.matcher(email).matches) => {
         email.setError(Html.fromHtml("<font color='red'>enter a valid email address</font>"))
@@ -92,4 +115,52 @@ object LoginView {
     })
     erroneous.isEmpty
   }
+}
+//
+import freestyle._
+import freestyle.implicits._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import cats.implicits._
+object freestyleO {
+  implicit val validationHandler = new Validation.Handler[Option] {
+    override def minSize(s: String, n: Int): Option[Boolean] = Option(s.size >= n)
+    override def hasNumber(s: String): Option[Boolean] = Option(s.exists(c => "0123456789".contains(c)))
+  }
+
+  implicit val interactionHandler = new Interaction.Handler[Option] {
+    override def tell(s: String): Option[Unit] = Option(println(s))
+    override def ask(s: String): Option[String] = Option("This could have been user input 1")
+  }
+
+  def program[F[_]](implicit A: Application[F]) = {
+    import A._
+    import cats.implicits._
+
+    for {
+      userInput <- interaction.ask("Give me something with at least 3 chars and a number on it")
+      valid <- (validation.minSize(userInput, 3), validation.hasNumber(userInput)).mapN(_ && _).freeS
+      _ <- if (valid)
+        interaction.tell(s"awesomesauce! '$userInput' is completely valid!")
+      else
+        interaction.tell(s"$userInput is not valid")
+    } yield ()
+  }
+
+  val futureValue = program[Application.Op].interpret[Option]
+}
+
+@free trait Validation {
+  def minSize(s: String, n: Int): FS[Boolean]
+  def hasNumber(s: String): FS[Boolean]
+}
+
+@free trait Interaction {
+  def tell(msg: String): FS[Unit]
+  def ask(prompt: String): FS[String]
+}
+
+@module trait Application {
+  val validation: Validation
+  val interaction: Interaction
 }
