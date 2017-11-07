1576bd2f02ffa8f337f2bd787211f5cd30d9dbdd
Artsiom <artkostm@gmail.com>
2017-11-07 16:28:54 +0300

    add abstract algebra

--- src/main/scala/com/artkostm/flymer/communication/login/algebras.scala
+++ src/main/scala/com/artkostm/flymer/communication/login/algebras.scala
@@ -1,8 +1,35 @@
 package com.artkostm.flymer.communication.login
 
+import freestyle._
+import org.jsoup.Connection
+import org.jsoup.nodes.Document
+
 /**
-  * Created by arttsiom.chuiko on 07/11/2017.
+  * Created by artsiom.chuiko on 07/11/2017.
   */
 object algebras {
+  type Keys = (String, String, String)
+  type Account = (String, String)
 
+  def extractAttribute(doc: Document, cssSelector: String, attrName: String): String =
+    doc.select(cssSelector).first.attr(attrName)
 }
+
+import algebras._
+@free trait PageLoader {
+  def loadLoginPage(): FS[Connection]
+  def loadAccount(account: Account, keys: Keys, cookies: Map[String, String]): FS[String]
+}
+
+@free trait PageDataExtractor {
+  def extractDocument(connection: Connection): FS[Document]
+  def extractCookies(connection: Connection): FS[Map[String, String]]
+}
+
+@free trait KeyResolver {
+  def getDkey(fkey: String): FS[String]
+  def getFkey(doc: Document): FS[String]
+  def getLkey(doc: Document): FS[String]
+}
+
+case class LoginInfo(ac: String, fkey: String, sid: String)
